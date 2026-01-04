a57def236a0423e31692ea9d57336040ac0925ea
Sabbir Hossain <sm.sabbir9671@gmail.com>
2026-01-04 17:02:12 +0600

    Finding the country from the API

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,16 @@
+import React, { use } from 'react';
+
+const Countries = ({countriesPromise}) => {
+    const countriesData = use(countriesPromise);
+    const countries = countriesData.countries;
+    console.log(countries);
+    
+    return (
+        <div>
+            <h1>In the Countries: {countries.length}</h1>
+        </div>
+    );
+};
+
+export default Countries;
+
